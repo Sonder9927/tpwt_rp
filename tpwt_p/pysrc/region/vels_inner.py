ff815c968eed7bde3437c7d284a06155d771615a
Sonder Merak <merak19981999@gmail.com>
2022-10-14 14:42:22 +0800

    new inside test

--- tpwt_p/pysrc/region/vels_inner.py
+++ tpwt_p/pysrc/region/vels_inner.py
@@ -1,6 +1,6 @@
 from scipy.spatial import ConvexHull
 from icecream import ic
-import tpwt_r
+from tpwt_r import Point
 import numpy as np
 import pandas as pd
 
@@ -21,44 +21,19 @@ def get_sta_boundary_points(f):
     return points[hull.vertices]
 
 
-# write this function to a method of class Point using rust
-# see my tpwt_r/src/lib.rs
-# def is_ray_intersects_segment(p, p1, p2):
-#     """
-#     check two lines of `y=p.la` and line crossing p1 and p2.
-#     """
-#     special_case: list[bool] = [
-#         p1.la == p2.la,
-#         (p1.la >= p.la) and (p2.la >= p.la),
-#         (p1.la <= p.la) and (p2.la <= p.la),
-#         (p1.lo < p.lo) and (p2.lo < p.lo),
-#     ]
-#     if any(special_case):
-#         return False
-
-#     x_position = p2.lo - (p2.lo - p1.lo) * (p2.la - p.la) / (p2.la - p1.la)
-#     if x_position < p.lo:
-#         return False
-
-#     return True
-
-
 def times_of_crossing_boundary(point, points):
-    times_r = 0
-    # times_p = 0
+    times = 0
     for i in range(len(points)):
-        r_start = points[i]
+        segment_start = points[i]
         if i == len(points)-1:
-            r_end = points[0]
+            segment_end = points[0]
         else:
-            r_end = points[i+1]
+            segment_end = points[i+1]
 
-        if point.is_ray_intersects_segment(r_start, r_end):
-            times_r += 1
-        # if is_ray_intersects_segment(point, r_start, r_end):
-        #     times_p += 1
+        if point.is_ray_intersects_segment(segment_start, segment_end):
+            times += 1
 
-    return times_r
+    return times
 
 
 def get_inter_points(grid_file: str, points: list):
@@ -78,7 +53,7 @@ def get_inter_points(grid_file: str, points: list):
 
     point_inner = []
     for i in vels_in_rect.index:
-        point = tpwt_r.Point(vels_in_rect.lo[i], vels_in_rect.la[i])
+        point = Point(vels_in_rect.lo[i], vels_in_rect.la[i])
 
         times = times_of_crossing_boundary(point, points)
         if times % 2 == 1:
@@ -94,13 +69,13 @@ def main(grid_dir, sta_file):
     boundary_points = get_sta_boundary_points(sta_file)  # default is clock
     # po = clock_sorted(boundary_points)  # no need
 
-    points = [tpwt_r.Point(p[0], p[1]) for p in boundary_points]
+    points = [Point(p[0], p[1]) for p in boundary_points]
     data_inner = get_inter_points(grid_dir, points)
     # ic(data_inner)
 
     total_numbers = len(data_inner.index)
     avgvel = data_inner.vel.mean()
-    ic(avgvel, total_numbers)
+    ic(total_numbers, avgvel)
 
 
 if __name__ == '__main__':
