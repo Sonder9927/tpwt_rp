e5fb84df434652005d376b4bf8a341e971b48c48
Sonder Merak <merak19981999@gmail.com>
2023-08-11 15:39:32 +0800

    no temp file when gmt

--- tpwt/tpwt_p/gmt/grid.py
+++ tpwt/tpwt_p/gmt/grid.py
@@ -2,16 +2,14 @@ import pygmt
 
 
 def grid_sample(data, region, spacing=0.5):
-    temp = "temp"
     # blockmean
-    pygmt.blockmean(data, outfile=temp, region=region, spacing=0.5)
+    temp = pygmt.blockmean(data, region=region, spacing=0.5)
     # surface
-    pygmt.surface(data=temp, outgrid=temp, region=region, spacing=0.5)
+    temp = pygmt.surface(data=temp, region=region, spacing=0.5)
     # grdsample
-    pygmt.grdsample(
+    temp = pygmt.grdsample(
         grid=temp,
         spacing=spacing,
-        outgrid=temp,
     )
     data = pygmt.grd2xyz(temp)
     return data
