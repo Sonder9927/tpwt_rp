33e0f1382aa8675f83188485b75c84f004ba1a80
Sonder Merak <merak19981999@gmail.com>
2023-10-21 16:47:05 +0800

    evt extract

--- tpwt/tpwt_p/tpwt_flow/evt_make/evt_maker.py
+++ tpwt/tpwt_p/tpwt_flow/evt_make/evt_maker.py
@@ -1,32 +1,22 @@
 from icecream import ic
 import datetime as dt
+from icecream.coloring import Error
 import pandas as pd
 
 
 class Evt_Make:
-    def __init__(self, evt1, evt2):
-        self.evt1 = pd.read_csv(
-            evt1, usecols=["time", "latitude", "longitude", "depth"]
-        )
-        self.evt2 = pd.read_csv(
-            evt2, usecols=["time", "latitude", "longitude", "depth"]
-        )
-        self.state = "NONE"
+    def __init__(self, evt1, evt2, time_delta):
+        self.pattern = "%Y-%m-%dT%H:%M:%S"
+        self.header = ["time", "latitude", "longitude", "depth", "mag"]
+        self.evt1 = pd.read_csv(evt1, usecols=self.header)
+        self.evt2 = pd.read_csv(evt2, usecols=self.header)
+        self._cut_time_delta(time_delta)
         ic("Hello, this is EVT maker.")
 
-    def info(self):
-        return f"The state of evt is {self.state},\nevt:\n{self.evt}"
-
-    def concat(self):
-        self.evt = pd.concat([self.evt1, self.evt2]).drop_duplicates(keep=False)
-        self.state = "CONCATTED"
-
-    def cut_time_delta(self, time_delta):
-        if self.state != "CONCATTED":
-            ic(f"evt is {self.state}, not CONCATTED.")
-            return
-
-        evt = self.evt
+    def _cut_time_delta(self, time_delta):
+        evt = pd.concat([self.evt1, self.evt2]).drop_duplicates(keep=False)
+        if evt is None:
+            raise ValueError
         evt.index = pd.to_datetime(evt["time"])
         temp = sorted(evt.index)
 
@@ -34,23 +24,21 @@ class Evt_Make:
         for i in range(len(temp) - 1):
             du = (temp[i + 1] - temp[i]).total_seconds()
             if du < time_delta:
-                drop_lst.update(temp[i : i + 2])
+                drop_lst.update(temp[i: i + 2])
 
         self.evt = evt.drop(drop_lst)
 
-    def evt_cat(self, cat, pattern, form):
-        evt = self.evt["time"].apply(lambda x: time_convert(str(x)[:19], pattern, form))
-        evt.to_csv(cat, sep=" ", header=False, index=False)
-        ic("Sucessfully get", cat)
-
-    def evt_lst(self, lst, pattern, form):
-        evt = self.evt
+    def extract(self, hn, form, fn):
+        evt = self.evt[self.header[:hn]]
         evt["time"] = evt["time"].apply(
-            lambda x: time_convert(str(x)[:19], pattern, form)
+            lambda x: time_convert(str(x)[:19], self.pattern, form)
         )
-        evt[["longitude", "latitude"]] = self.evt[["latitude", "longitude"]]
-        evt.to_csv(lst, sep=" ", header=False, index=False)
-        ic("Sucessfully get", lst)
+        if hn >= 3:
+            evt[["longitude", "latitude"]] = self.evt[
+                ["latitude", "longitude"]
+            ]
+        evt.to_csv(fn, sep=" ", header=False, index=False)
+        ic("Sucessfully get", fn)
 
 
 def time_convert(val: str, pattern: str, form: str) -> str:
